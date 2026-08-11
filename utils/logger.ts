786ae99cfe0b94bd73eaa174026509ed8eaf6d8b
“Adenubi <habeebolamide591@gmail.com>
2026-08-11 20:23:00 +0100

    fix

--- utils/logger.ts
+++ utils/logger.ts
@@ -10,6 +10,7 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: 'logs/combined.log' }),
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.Console(),
   ],
 });
 
